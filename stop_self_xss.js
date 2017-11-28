6f62fb4ad9605e55f49b125cf94023a240f2663c
Matthew Paul Gross <MatthewGross@users.noreply.github.com>
2017-11-28 14:46:15 -0500

    Update and rename stop_self_xss.html to stop_self_xss.js

---
+++ stop_self_xss.js
@@ -0,0 +1,4 @@
+if(typeof window.stopSelfXSSWarning == 'undefined' || window.stopSelfXSSWarning == false) {
+    console.log("%cStop!", "font: 3em sans-serif; color: red; background-color: white;");
+    console.log("%cThis is a browser feature intended for developers. If someone told you to copy-paste something here to enable a feature, it is a scam!", "font: 1.5em sans-serif; color: grey;");
+}
